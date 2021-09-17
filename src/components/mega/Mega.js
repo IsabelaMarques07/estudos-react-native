4e1644fd96926e6239df0fbe9b9b99c6b1d6605b
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2021-09-17 18:38:00 -0300

    Gerador de números de Mega-Sena

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import { Text, TextInput } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
 import Estilo from '../estilo'
+import Numero from './Numero';
 
 export default class Mega extends Component{
 
@@ -24,12 +25,44 @@ export default class Mega extends Component{
 
 
     state = {
-        qtdeNumeros: this.props.qtdeNumeros
+        qtdeNumeros: this.props.qtdeNumeros,
+        numeros: []
     }
 
     //Resolver o problema do this
     alterarQtdeNumero = (qtde)=> {
-        this.setState({ qtdeNumeros: qtde })
+        this.setState({ qtdeNumeros: +qtde }) 
+    }
+
+    gerarNumeroNaoContido = nums => {
+        const novo = parseInt(Math.random() * 60) + 1
+        return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo
+    }
+
+    gerarNumeros = () => {
+        const numeros = Array(this.state.qtdeNumeros)
+                        .fill() 
+                        .reduce(nums => [...nums, this.gerarNumeroNaoContido(nums)], [])
+                        .sort((a, b)=> a - b)
+        this.setState({ numeros })
+    }
+
+    //Outra alternativa
+    // gerarNumeros = () => {
+    //     const {qtdeNumeros} = this.state
+    //     const numeros = []
+    //     for(let i = 0; i < qtdeNumeros; i++){
+    //         numeros.push(this.gerarNumeroNaoContido(numeros))
+    //     }
+    //     numeros.sort((a, b)=> a-b)
+    //     this.setState({ numeros })
+    // }
+
+    exibirNumeros = () =>{
+        const nums = this.state.numeros
+        return nums.map(num =>{
+            return <Numero key={num} num={num}/>
+        })
     }
 
     render(){
@@ -37,17 +70,30 @@ export default class Mega extends Component{
             <>
                 <Text style={Estilo.txtG}>
                     Gerador de Mega-Sena 
-                    {this.state.qtdeNumeros}
+                    
                 </Text>
                 <TextInput
-                    keyboardType={'number-pad'}
+                    keyboardType={'numeric'}
                     style={{borderBottomWidth: 1}}
                     placeholder="Qtde de números"
-                    value={this.state.qtdeNumeros}
+                    value={`${this.state.qtdeNumeros}`}
                     onChangeText={qtde => this.alterarQtdeNumero(qtde)}
                     // Outra forma de resolver o problema do this
                     // onChangeText={qtde => this.alterarQtdeNumero(qtde)}
                 />
+                <Button
+                    title="Gerar números"
+                    onPress={this.gerarNumeros}
+                />
+                <View style={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                    marginTop: 20
+                }}>
+                    {this.exibirNumeros()}
+                </View>
+
             </>
         )
     }
